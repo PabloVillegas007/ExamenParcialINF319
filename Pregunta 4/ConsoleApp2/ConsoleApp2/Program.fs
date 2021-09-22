9a3517223b7521e7821345c5e95eaaf726fba6af
PabloVillegas007 <91212674+PabloVillegas007@users.noreply.github.com>
2021-09-22 14:58:25 -0400

    Add files via upload

---
+++ Pregunta 4/ConsoleApp2/ConsoleApp2/Program.fs
@@ -0,0 +1,28 @@
+﻿// Learn more about F# at http://fsharp.org
+open System
+[<EntryPoint>]
+let main argv =
+    
+    //introduciendo numeros del 1 al 20 con un for
+    let lista = [for i in 0..20 -> i]
+    //introduciendo numeros de forma manual
+    //let lista = [15;33;28;48;51;3;1;14;52;84;99;75;63]
+    
+    let mutable lpares = []
+    let mutable lipares = []
+    for i in lista do
+        if (i % 2 = 0) then
+            lpares <- List.append lpares [i] 
+        else 
+            lipares <- List.append lipares [i] 
+
+    printf "Numeros Pares: "
+    for i in lpares do
+        printf "%i " i
+    printfn ""
+    printf "Numeros Impares: "
+    for i in lipares do
+        printf "%i " i
+    
+    System.Console.ReadKey() |>ignore
+    0 // return an integer exit code
